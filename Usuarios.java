f09e8e4de90594e4165b18a82f448728874a7519
Vanessa Aldrighi <aldrighivanessa@gmail.com>
2025-06-22 21:56:59 -0300

    Q8 Usuario e Usuarios

---
+++ Usuarios.java
@@ -0,0 +1,8 @@
+public class Usuarios {
+    public static void main(String[] args) {
+        Usuario u = new Usuario("Vanessa", 24);
+
+        System.out.println("Nome: " + u.nome);
+        System.out.println("Idade: " + u.idade);
+    }
+}
